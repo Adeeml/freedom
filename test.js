e95f42d16455ce81e48ccc8442a0d9ca5c7d6dcb
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2021-12-15 12:33:48 -0600

    Testing code from `discordjs docs`

---
+++ test.js
@@ -0,0 +1,51 @@
+const Keyv = require('keyv');
+const { Client, Intents } = require('discord.js');
+const { globalPrefix } = require('./configs/config.json');
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+const prefixes = new Keyv(`${process.env.MONGO_URL}`);
+
+client.once('ready', () => {
+	console.log('Ready!'),
+    client.user.setActivity('test', { type: 'PLAYING'});
+});
+
+client.on('messageCreate', async message => {
+	if (message.author.bot) return;
+
+	let args;
+	if (message.guild) {
+		let prefix;
+
+		if (message.content.startsWith(globalPrefix)) {
+			prefix = globalPrefix;
+		} else {
+			const guildPrefix = await prefixes.get(message.guild.id);
+			if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
+		}
+
+		if (!prefix) return;
+		args = message.content.slice(prefix.length).trim().split(/\s+/);
+	} else {
+		const slice = message.content.startsWith(globalPrefix) ? globalPrefix.length : 0;
+		args = message.content.slice(slice).split(/\s+/);
+	}
+
+	const command = args.shift().toLowerCase();
+
+	if (command === 'prefix') {
+		if (args.length) {
+			await prefixes.set(message.guild.id, args[0]);
+			return message.channel.send(`Successfully set prefix to \`${args[0]}\``);
+		}
+
+		return message.channel.send(`Prefix is \`${await prefixes.get(message.guild.id) || globalPrefix}\``);
+	}
+});
+
+client.login(process.env.TOKEN);
