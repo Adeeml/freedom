8cf8b724711e0167c77cf5916762074ca423165c
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2021-12-15 14:57:14 -0600

    I might have actually fixed it...

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,12 +8,11 @@ const globalPrefix = config.globalPrefix;
 
 client.on('messageCreate', async (message) => {
 	// Ignore WebManagebot, DMs, partial messages in channels and themselves
-	/*
 	if (message.author.bot) return;
 	if (!message.guild) return message.reply('Please send commands in a server');
 	if (message.channel.partial) await message.channel.fetch();
 	if (message.partial) await message.fetch();
-*/
+	
 	let args;
 	// handle messages in a guild
 	if (message.guild) {
@@ -22,21 +21,18 @@ client.on('messageCreate', async (message) => {
 		if (message.content.startsWith(globalPrefix)) {
 			prefix = globalPrefix;
 		} else {
-			// check the guild-level prefix
 			const guildPrefix = await prefixes.get(message.guild.id);
 			if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
 		}
 
-		// if we found a prefix, setup args; otherwise, this isn't a command
 		if (!prefix) return;
 		args = message.content.slice(prefix.length).trim().split(/\s+/);
 	} else {
-		// handle DMs
 		const slice = message.content.startsWith(globalPrefix) ? globalPrefix.length : 0;
 		args = message.content.slice(slice).split(/\s+/);
 	}
 
-	// get the first space-delimited argument after the prefix as the command
-	const cmd = args.shift().toLowerCase();
-	const command = client.commands.get(cmd.toLowerCase());
+		const cmd = args.shift().toLowerCase();
+		const command = client.commands.get(cmd.toLowerCase());
+	await command.run(client, message, args, prefix)
 });
