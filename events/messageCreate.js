5cc47c4408170e916c58d4139a8397c016cced16
WebManageBotOFFICIAL <83777902+WebManageBotOFFICIAL@users.noreply.github.com>
2022-01-05 13:55:55 -0600

    Updated discord.js from v13.3.1 to latest

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,36 +7,37 @@ const config = require('../configs/config.json');
 const globalPrefix = config.globalPrefix;
 
 client.on('messageCreate', async (message) => {
-	// Ignore WebManagebot, DMs, partial messages in channels and themselves
+	//const { escapeRegex, onCoolDown } = require("../utils/function.js");
+// Ignore WebManagebot, DMs, partial messages in channels and themselves
 	if (message.author.bot) return;
 	if (!message.guild) return message.reply('Please send commands in a server');
 	if (message.channel.partial) await message.channel.fetch();
 	if (message.partial) await message.fetch();
 
 	let args;
-	const cmd = args.shift().toLowerCase();
-	const command = client.commands.get(cmd.toLowerCase());
-	// handle messages in a guild
+// Handle messages in a guild
 	if (message.guild) {
 		let prefix;
 
 		if (message.content.startsWith(globalPrefix)) {
 			prefix = globalPrefix;
 		} else {
+			// check the guild-level prefix
 			const guildPrefix = await prefixes.get(message.guild.id);
 			if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
 		}
 
+// If we found a prefix, setup args; otherwise, this isn't a command
 		if (!prefix) return;
 		args = message.content.slice(prefix.length).trim().split(/\s+/);
 	} else {
+// Handle DMs
 		const slice = message.content.startsWith(globalPrefix) ? globalPrefix.length : 0;
 		args = message.content.slice(slice).split(/\s+/);
 	}
-	if (!command) return;
-	if (command) {
-	  if (!message.member.permissions.has(command.permissions || []))
-		return message.channel.send('You do not have the perms to complete such actions...');
-	};
-	await command.run(client, message, args, prefix);
+
+// Get the first space-delimited argument after the prefix as the command
+	const command = args.shift().toLowerCase();
+// Send the command
+  await command.run(client, message, args, prefix);
 });
