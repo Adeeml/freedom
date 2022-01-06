377d33da5633fc2a4e7564f2fedc6b9c70dca4bd
WebManageBotOFFICIAL <83777902+WebManageBotOFFICIAL@users.noreply.github.com>
2022-01-06 13:55:33 -0600

    Made it so it replies last, now adding functionality

--- commands/server/nt.js
+++ commands/server/nt.js
@@ -5,14 +5,16 @@ module.exports = {
     description: 'nickname test',
     run: async (client, message, args) => {
     const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    let nNick = args.slice(2).join(" ");
+    let nNick = args.slice(1).join(" ");
     if (!user) return message.reply('Please mention a user to change');
-    if (!nNick) nNick = "No-Name"
+    if (!nNick) user.member.setNickname(message.content.replace('No-Name'));
+    if (nNick) nNick = user.member.setNickname(nNick);
 
         const nick = new MessageEmbed()
           .setTitle('Nickname changed!')
-          //.addField('Original', )
-          .addField('New |', `${nNick}`)
+          .addField('Original', `${user}`)
+          .addField('New', `${nNick}`)
+          .setFooter('Someone\'s name was changed...')
     
       message.reply({ embeds: [nick] });
     }
