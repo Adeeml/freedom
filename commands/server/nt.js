888c02ee10d563889d79c089f719019cdf701513
WebManageBotOFFICIAL <83777902+WebManageBotOFFICIAL@users.noreply.github.com>
2022-01-06 11:00:23 -0600

    Test nickname code I found on internet

---
+++ commands/server/nt.js
@@ -0,0 +1,22 @@
+const { Message, MessageEmbed, Collection } = require('discord.js');
+
+module.exports = {
+    name: 'nt',
+    description: 'nickname test',
+    run: async (client, message, args) => {
+    const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    let nNick = args.slice(2).join(" ");
+    if (!user) return message.reply('Please mention a user to change');
+    if (!nick) nNick = "No-Name"
+
+        const nick = new MessageEmbed()
+          .setTitle('Nickname changed!')
+          .addField('Original', )
+          .addField('New |', `${nick}`)
+    
+      message.reply({
+        content: 'Nick',
+        embed: [nNick]
+    });
+    }
+}
